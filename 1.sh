11dabded45a5ba61bc961f8ca4df8dfdcf386148
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-08-24 18:53:34 -0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 apt update ;
+
 apt upgrade -y ;
+
 apt install curl openssh-server -y ;
+
+systemctl start ssh ;
+
+systemctl enable ssh ;
+
 curl -sSL https://install.pi-hole.net | bash ;
+
 pihole -a -p DEBIVER ;

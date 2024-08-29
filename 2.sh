c7ced0f731055917e20d737073145747e0a8bc96
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-08-29 16:58:28 -0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,70 +1,32 @@
 #!/bin/bash
 
-
-### CRIAR USUARIO ROOT COM NOME DE "4RCH"
 echo 4RCH > /etc/hostname;
 
 
-
-
-
-### CRIAR SENHA COM NOME DE "4RCH" DO USUÁRIO ROOT
 yes 4RCH | passwd root;
 
 
-
-
-
-### CRIAR USUÁRIO NORMAL COM NOME DE "4RCH"
 useradd -m -g users -G wheel 4RCH;
 
 
-
-
-
-### CRIAR SENHA COM NOME DE "4RCH" DO USUARIO NORMAL
 yes 4RCH | passwd 4RCH;
 
 
-
-
-
-### REMOVER E ADICIONAR NOVO PARAMETRO DE IDIOMA, DATA, HORARIO E FUSO HORARIO PARA NACIONALIDADE BRASILEIRA
 echo "pt_BR.UTF-8 UTF-8" > /etc/locale.gen;
 
 
-
-
-
-### REMOVER E ADICIONAR NOVO PARAMETRO DE LOCALIZAÇÃO PARA BRASILERA
 echo "LANG=pt_BR.UTF-8" > /etc/locale.conf;
 
 
-
-
-
-### ATUALIZAR ARQUIVO DE CONFIGURAÇÕES REGIONAIS
 locale-gen;
 
 
-
-
-
-### SINCRONIZAR E ATUALIZAR RELOGIO
 hwclock --systohc;
 
 
-
-
-
-### SOBRESCREVER ARQUIVO "mirrorlist" EM "/etc/pacman.d"
 echo "Server=https://mirror.ufscar.br/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist;
 
 
-
-
-
-### SOBRESCREVER ARQUIVO ".bashrc" EM "/mnt/home/4RCH"
 echo "alias i='yay -S --noconfirm --quiet'
 alias d='sudo pacman -Rsc'
 sudo rm -rf /home/4RCH/.bash_history;
@@ -78,9 +40,6 @@ fastfetch
 sudo chmod 777 SERVIDOR.sh && ./SERVIDOR.sh && sed -i '\$d' /home/4RCH/.bashrc" > /home/4RCH/.bashrc;
 
 
-
-
-### CRIAR ARQUIVO SCRIPT PARA INSTALAR PACOTES PARA SERVIDOR
 echo "\#\!/bin/bash
 git clone https://aur.archlinux.org/yay.git;
 sudo chmod 777 yay;
@@ -96,10 +55,6 @@ sudo systemctl start pihole-FTL;
 sudo rm -rf SERVIDOR.sh" > /home/4RCH/SERVIDOR.sh;
 
 
-
-
-
-### SOBRESCREVER ARQUIVO "pacman.conf" EM "/etc"
 echo "[options]
 Architecture=auto
 CheckSpace
@@ -116,43 +71,24 @@ Include=/etc/pacman.d/mirrorlist
 Include=/etc/pacman.d/mirrorlist" > /etc/pacman.conf;
 
 
-
-
-
-### SINCRONIZAR ESPELHO E ATUALIZAR PACOTES
 pacman -Syyu --noconfirm --quiet;
 
 
-
-
-
-### INSTALAR DRIVERS AMD
 if lspci | grep -i amd; then
 pacman -Sy --noconfirm amd-ucode
 fi;
 
 
-
-
-
-### INSTALAR DRIVERS INTEL
 if lspci | grep -i intel; then
 pacman -Sy --noconfirm intel-ucode
 fi;
 
 
-
-
-
-### INSTALAR DRIVERS NVIDIA
 if lspci | grep -i nvidia; then
 pacman -Sy --noconfirm nvidia nvidia-dkms
 fi;
 
 
-
-
-### INSTALAR DRIVERS VIRTUALBOX
 if lspci | grep -i virtualbox; then
 pacman -Sy --noconfirm \
 virtualbox-guest-utils \
@@ -161,34 +97,18 @@ systemctl enable vboxservice;
 fi;
 
 
-
-
-
-### HABILITAR AUTOINICIALIZACAO DE SERVICOS
 systemctl enable NetworkManager
 
 
-
-
-
-### DESABILITAR AUTOINICIALIZACAO DE SERVICOS
 systemctl disable \
 NetworkManager-wait-online \
 systemd-networkd \
 systemd-timesyncd;
 
 
-
-
-
-### RECONSTRUIR IMAGEM DO INITRAMFS USANDO CONFIGURACOES PADRAO DEFINIDAS PARA O PROCESSO DE INICIALIZACAO DO SISTEMA
 mkinitcpio -P;
 
 
-
-
-
-### SOBRESCREVER ARQUIVO "grub" EM "/etc/default"
 echo "GRUB_DEFAULT=0
 GRUB_TIMEOUT=0
 GRUB_DISTRIBUTOR=\"4RCH\"
@@ -200,23 +120,13 @@ GRUB_GFXPAYLOAD_LINUX=keep
 GRUB_DISABLE_RECOVERY=true" > /etc/default/grub;
 
 
-
-
-
-### INSTALAR GRUB
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=4RCH --recheck;
 
 
-
-
-
-### GERAR UM NOVO ARQUIVO DE CONFIGURACAO PARA O GRUB
 grub-mkconfig -o /boot/grub/grub.cfg;
 
 
 
-
-### CONFIGURACOES ADICIONAIS
 echo "4RCH ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers;
 
 sed -i "/^\s*#/d; /^\s*$/d" \

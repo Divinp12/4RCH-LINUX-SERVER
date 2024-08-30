04fd7a7fc3a81bb10b024e4bd69374bd2c7401ee
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-08-30 17:08:17 -0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -15,8 +15,6 @@ sudo pacman -Syyu --noconfirm --quiet;
 sudo pacman -Scc --noconfirm --quiet;
 clear;
 fastfetch
-sudo chmod 777 SERVIDOR.sh && ./SERVIDOR.sh && sed -i '\$d' /home/4RCH/.bashrc" > /home/4RCH/.bashrc;
-echo "\#\!/bin/bash
 git clone https://aur.archlinux.org/yay.git;
 sudo chmod 777 yay;
 cd yay;
@@ -30,44 +28,9 @@ sudo systemctl enable sshd;
 sudo systemctl start sshd;
 sudo systemctl enable pihole-FTL;
 sudo systemctl start pihole-FTL;
-sudo echo 'https://blocklistproject.github.io/Lists/abuse.txt
-https://blocklistproject.github.io/Lists/alt-version/abuse-nl.txt
-https://blocklistproject.github.io/Lists/ads.txt
-https://blocklistproject.github.io/Lists/alt-version/ads-nl.txt
-https://blocklistproject.github.io/Lists/crypto.txt
-https://blocklistproject.github.io/Lists/alt-version/crypto-nl.txt
-https://blocklistproject.github.io/Lists/drugs.txt
-https://blocklistproject.github.io/Lists/alt-version/drugs-nl.txt
-https://blocklistproject.github.io/Lists/everything.txt
-https://blocklistproject.github.io/Lists/alt-version/everything-nl.txt
-https://blocklistproject.github.io/Lists/facebook.txt
-https://blocklistproject.github.io/Lists/alt-version/facebook-nl.txt
-https://blocklistproject.github.io/Lists/fraud.txt
-https://blocklistproject.github.io/Lists/alt-version/fraud-nl.txt
-https://blocklistproject.github.io/Lists/gambling.txt
-https://blocklistproject.github.io/Lists/alt-version/gambling-nl.txt
-https://blocklistproject.github.io/Lists/malware.txt
-https://blocklistproject.github.io/Lists/alt-version/malware-nl.txt
-https://blocklistproject.github.io/Lists/phishing.txt
-https://blocklistproject.github.io/Lists/alt-version/phishing-nl.txt
-https://blocklistproject.github.io/Lists/piracy.txt
-https://blocklistproject.github.io/Lists/alt-version/piracy-nl.txt
-https://blocklistproject.github.io/Lists/porn.txt
-https://blocklistproject.github.io/Lists/alt-version/porn-nl.txt
-https://blocklistproject.github.io/Lists/ransomware.txt
-https://blocklistproject.github.io/Lists/alt-version/ransomware-nl.txt
-https://blocklistproject.github.io/Lists/redirect.txt
-https://blocklistproject.github.io/Lists/alt-version/redirect-nl.txt
-https://blocklistproject.github.io/Lists/scam.txt
-https://blocklistproject.github.io/Lists/alt-version/scam-nl.txt
-https://blocklistproject.github.io/Lists/tiktok.txt
-https://blocklistproject.github.io/Lists/alt-version/tiktok-nl.txt
-https://blocklistproject.github.io/Lists/torrent.txt
-https://blocklistproject.github.io/Lists/alt-version/torrent-nl.txt
-https://blocklistproject.github.io/Lists/tracking.txt
-https://blocklistproject.github.io/Lists/alt-version/tracking-nl.txt' \> /etc/pihole/adlists.list;
 pihole -g -r recreate;
-sudo rm -rf SERVIDOR.sh" > /home/4RCH/SERVIDOR.sh;
+sed -i '\$d' /home/4RCH/.bashrc" > /home/4RCH/.bashrc;
+
 echo "[options]
 Architecture=auto
 CheckSpace
@@ -129,6 +92,42 @@ sed -i "/^\s*#/d; /^\s*$/d" \
 /etc/fuse.conf \
 /etc/ts.conf \
 /etc/fstab;
+echo "https://blocklistproject.github.io/Lists/abuse.txt
+https://blocklistproject.github.io/Lists/alt-version/abuse-nl.txt
+https://blocklistproject.github.io/Lists/ads.txt
+https://blocklistproject.github.io/Lists/alt-version/ads-nl.txt
+https://blocklistproject.github.io/Lists/crypto.txt
+https://blocklistproject.github.io/Lists/alt-version/crypto-nl.txt
+https://blocklistproject.github.io/Lists/drugs.txt
+https://blocklistproject.github.io/Lists/alt-version/drugs-nl.txt
+https://blocklistproject.github.io/Lists/everything.txt
+https://blocklistproject.github.io/Lists/alt-version/everything-nl.txt
+https://blocklistproject.github.io/Lists/facebook.txt
+https://blocklistproject.github.io/Lists/alt-version/facebook-nl.txt
+https://blocklistproject.github.io/Lists/fraud.txt
+https://blocklistproject.github.io/Lists/alt-version/fraud-nl.txt
+https://blocklistproject.github.io/Lists/gambling.txt
+https://blocklistproject.github.io/Lists/alt-version/gambling-nl.txt
+https://blocklistproject.github.io/Lists/malware.txt
+https://blocklistproject.github.io/Lists/alt-version/malware-nl.txt
+https://blocklistproject.github.io/Lists/phishing.txt
+https://blocklistproject.github.io/Lists/alt-version/phishing-nl.txt
+https://blocklistproject.github.io/Lists/piracy.txt
+https://blocklistproject.github.io/Lists/alt-version/piracy-nl.txt
+https://blocklistproject.github.io/Lists/porn.txt
+https://blocklistproject.github.io/Lists/alt-version/porn-nl.txt
+https://blocklistproject.github.io/Lists/ransomware.txt
+https://blocklistproject.github.io/Lists/alt-version/ransomware-nl.txt
+https://blocklistproject.github.io/Lists/redirect.txt
+https://blocklistproject.github.io/Lists/alt-version/redirect-nl.txt
+https://blocklistproject.github.io/Lists/scam.txt
+https://blocklistproject.github.io/Lists/alt-version/scam-nl.txt
+https://blocklistproject.github.io/Lists/tiktok.txt
+https://blocklistproject.github.io/Lists/alt-version/tiktok-nl.txt
+https://blocklistproject.github.io/Lists/torrent.txt
+https://blocklistproject.github.io/Lists/alt-version/torrent-nl.txt
+https://blocklistproject.github.io/Lists/tracking.txt
+https://blocklistproject.github.io/Lists/alt-version/tracking-nl.txt" > /etc/pihole/adlists.list;
 sed -i "/^UUID=.* \/boot .*$/! s/rw/rw,noatime,discard,/" /etc/fstab;
 echo "127.0.0.1 localhost.localdomain localhost
 ::1 localhost.localdomain localhost

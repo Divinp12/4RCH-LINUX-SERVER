caa97b1bf09cc8ecf1ee9d5f9c0a9484ac1919ca
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-09-02 15:56:42 -0300

    Update SSH.sh

--- PACOTES/SSH.sh
+++ PACOTES/SSH.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-yay -S --noconfirm --quiet openssh;
-
-sudo systemctl enable sshd;
-
+yay -S --noconfirm --quiet openssh && \
+sudo systemctl enable sshd && \
 sudo systemctl start sshd;

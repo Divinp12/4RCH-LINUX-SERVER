e7a6b28cb41cac019d74cd8e531c4540cd2a03ac
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-09-02 15:37:18 -0300

    Update YAY.sh

--- PACOTES/YAY.sh
+++ PACOTES/YAY.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-git clone https://aur.archlinux.org/yay.git;
-
-sudo chmod 777 yay;
-
-cd yay;
-
-makepkg -si --noconfirm;
-
-cd ..;
-
-sudo rm -rf yay;
-
+git clone https://aur.archlinux.org/yay.git && \
+sudo chmod 777 yay && \
+cd yay && \
+makepkg -si --noconfirm && \
+cd .. && \
+sudo rm -rf yay && \
 yay -S --noconfirm nano --save --answerdiff None --answerclean None --removemake;

49d5666dc26108303705442c18d44d2146d16eaa
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-09-21 17:26:09 -0300

    Update AUTOINSTALAÇÃO.sh

--- AUTOINSTALAÇÃO.sh
+++ AUTOINSTALAÇÃO.sh
@@ -148,7 +148,49 @@ cd yay && \\
 makepkg -si --noconfirm && \\
 cd .. && \\
 sudo rm -rf yay && \\
-yay -S --noconfirm nano --save --answerdiff None --answerclean None --removemake;" > /home/4RCH/.bashrc;
+yay -S --noconfirm nano --save --answerdiff None --answerclean None --removemake;
+
+yay -S --noconfirm --quiet pi-hole-server && \
+sudo systemctl enable pihole-FTL && \
+sudo systemctl start pihole-FTL && \
+pihole -g && \
+echo "https://blocklistproject.github.io/Lists/abuse.txt
+https://blocklistproject.github.io/Lists/alt-version/abuse-nl.txt
+https://blocklistproject.github.io/Lists/ads.txt
+https://blocklistproject.github.io/Lists/alt-version/ads-nl.txt
+https://blocklistproject.github.io/Lists/crypto.txt
+https://blocklistproject.github.io/Lists/alt-version/crypto-nl.txt
+https://blocklistproject.github.io/Lists/drugs.txt
+https://blocklistproject.github.io/Lists/alt-version/drugs-nl.txt
+https://blocklistproject.github.io/Lists/everything.txt
+https://blocklistproject.github.io/Lists/alt-version/everything-nl.txt
+https://blocklistproject.github.io/Lists/facebook.txt
+https://blocklistproject.github.io/Lists/alt-version/facebook-nl.txt
+https://blocklistproject.github.io/Lists/fraud.txt
+https://blocklistproject.github.io/Lists/alt-version/fraud-nl.txt
+https://blocklistproject.github.io/Lists/gambling.txt
+https://blocklistproject.github.io/Lists/alt-version/gambling-nl.txt
+https://blocklistproject.github.io/Lists/malware.txt
+https://blocklistproject.github.io/Lists/alt-version/malware-nl.txt
+https://blocklistproject.github.io/Lists/phishing.txt
+https://blocklistproject.github.io/Lists/alt-version/phishing-nl.txt
+https://blocklistproject.github.io/Lists/piracy.txt
+https://blocklistproject.github.io/Lists/alt-version/piracy-nl.txt
+https://blocklistproject.github.io/Lists/porn.txt
+https://blocklistproject.github.io/Lists/alt-version/porn-nl.txt
+https://blocklistproject.github.io/Lists/ransomware.txt
+https://blocklistproject.github.io/Lists/alt-version/ransomware-nl.txt
+https://blocklistproject.github.io/Lists/redirect.txt
+https://blocklistproject.github.io/Lists/alt-version/redirect-nl.txt
+https://blocklistproject.github.io/Lists/scam.txt
+https://blocklistproject.github.io/Lists/alt-version/scam-nl.txt
+https://blocklistproject.github.io/Lists/tiktok.txt
+https://blocklistproject.github.io/Lists/alt-version/tiktok-nl.txt
+https://blocklistproject.github.io/Lists/torrent.txt
+https://blocklistproject.github.io/Lists/alt-version/torrent-nl.txt
+https://blocklistproject.github.io/Lists/tracking.txt
+https://blocklistproject.github.io/Lists/alt-version/tracking-nl.txt" >> /etc/pihole/adlists.list && \
+pihole -g -r recreate;" > /home/4RCH/.bashrc;
 
 echo "[options]
 Architecture=auto
